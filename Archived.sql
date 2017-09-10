9b984587e58fe0ee280fc2e821cbe6b53887b5e2
Muhammed Jobe <thejobemuhammed@gmail.com>
2017-09-10 15:02:18 -0400

    Create Archived.sql

---
+++ Archived.sql
@@ -0,0 +1,154 @@
+INSERT INTO content_video
+(
+  name,
+  artist,
+  url,
+  description,
+  shots,
+  date_added,
+  date_updated
+)
+VALUES
+(
+  'The Sleeperz Part 1',
+  'tourmonkeys',
+  'X3BF7Q2M',
+  'The Sleeperz Part 1',
+  'note',
+  TIMESTAMP '2017-09-09 16:42:18.698',
+  TIMESTAMP '2017-09-09 16:42:18.698'
+);
+
+
+INSERT INTO content_video
+(
+  name,
+  artist,
+  url,
+  description,
+  shots,
+  date_added,
+  date_updated
+)
+VALUES
+(
+  'The Sleeperz Part 2',
+  'tourmonkeys',
+  'JyGut0A8',
+  'The Sleeperz Part 2',
+  'note',
+  TIMESTAMP '2017-09-09 16:42:22.711',
+  TIMESTAMP '2017-09-09 16:42:22.711'
+);
+
+
+INSERT INTO content_video
+(
+  name,
+  artist,
+  url,
+  description,
+  shots,
+  date_added,
+  date_updated
+)
+VALUES
+(
+  'The Sleeperz Part 3',
+  'tourmonkeys',
+  'aHjlLkZk',
+  'The Sleeperz Part 3',
+  'note',
+  TIMESTAMP '2017-09-09 16:42:32.095',
+  TIMESTAMP '2017-09-09 16:42:32.095'
+);
+
+
+INSERT INTO content_video
+(
+  name,
+  artist,
+  url,
+  description,
+  shots,
+  date_added,
+  date_updated
+)
+VALUES
+(
+  'The Sleeperz Part 4',
+  'tourmonkeys',
+  'EFHts0uN',
+  'The Sleeperz Part 4',
+  'note',
+  TIMESTAMP '2017-09-09 16:45:14.201',
+  TIMESTAMP '2017-09-09 16:45:14.201'
+);
+
+
+INSERT INTO content_video
+(
+  name,
+  artist,
+  url,
+  description,
+  shots,
+  date_added,
+  date_updated
+)
+VALUES
+(
+  'The Sleeperz Part 5',
+  'tourmonkeys',
+  'jd4UQock',
+  'The Sleeperz Part 4',
+  'note',
+  TIMESTAMP '2017-09-09 16:45:18.649',
+  TIMESTAMP '2017-09-09 16:45:18.649'
+);
+
+
+INSERT INTO content_video
+(
+  name,
+  artist,
+  url,
+  description,
+  shots,
+  date_added,
+  date_updated
+)
+VALUES
+(
+  'TYTGS Part 1',
+  'tourmonkeys',
+  'kMXgRgmP',
+  'TYTGS Part 1',
+  'note',
+  TIMESTAMP '2017-09-09 16:45:27.098',
+  TIMESTAMP '2017-09-09 16:45:27.098'
+);
+
+
+INSERT INTO content_video
+(
+  name,
+  artist,
+  url,
+  description,
+  shots,
+  date_added,
+  date_updated
+)
+VALUES
+(
+  'TYTGS Part 2',
+  'tourmonkeys',
+  '6VdhYp4T',
+  'TYTGS Part 2',
+  'note',
+  TIMESTAMP '2017-09-09 16:48:22.126',
+  TIMESTAMP '2017-09-09 16:48:22.126'
+);
+
+
